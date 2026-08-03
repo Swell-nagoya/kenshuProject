79e3d9c07abddd25255c26e5350c91b1db3bd840
swllns <n.sakamoto@swell.inc>
2026-08-03 14:20:41 +0900

    ログイン画面の修正

--- src/jp/swell/dao/UserReserveDao.java
+++ src/jp/swell/dao/UserReserveDao.java
@@ -241,16 +241,17 @@ public class UserReserveDao implements Serializable {
    * @param dao UserReserveDaoこのテーブルのインスタンス
    */
   public void setUserReserveDaoForJoin(HashMap<String, String> map, UserReserveDao dao) throws AtareSysException {
-    dao.setUserReserveId(DbI.chara(map.get("user_reserve___user_reserve_id")));
-    dao.setUserInfoId(DbI.chara(map.get("user_reserve___user_info_id")));
-    dao.setReserveId(DbI.chara(map.get("user_reserve___reserve_id")));
+    System.out.println("★Mapのキー一覧: " + map.keySet());
+    dao.setUserReserveId(DbI.chara(map.get("user_reserve_id")));
+    dao.setUserInfoId(DbI.chara(map.get("user_info_id")));
+    dao.setReserveId(DbI.chara(map.get("reserve_id")));
     // link_user_idに対応するreserve_idを取得し、セット
     ReserveDao reserve = new ReserveDao();
-    reserve.dbSelect(DbI.chara(map.getOrDefault("user_reserve___reserve_id", "")));
+    reserve.dbSelect(DbI.chara(map.getOrDefault("reserve_id","")));
     dao.setReserveDaos(reserve);
     // link_user_idに対応するreserve_idを取得し、セット
     ReserveFileDao reserveFile = new ReserveFileDao();
-    reserveFile.dbSelect(DbI.chara(map.getOrDefault("user_reserve___reserve_id", "")));
+    reserveFile.dbSelect(DbI.chara(map.getOrDefault("reserve_id", "")));
     dao.setReserveFileDaos(reserveFile);
   }
   /**
